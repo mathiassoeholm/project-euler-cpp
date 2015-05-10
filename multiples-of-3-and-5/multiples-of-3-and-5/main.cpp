96ebd6fc17667923f5da392d328ebd9a8600d22b
Mathias Soeholm <mathiassoeholm@gmail.com>
2015-05-10 19:57:35 +0200

    Finished 1st problem

--- multiples-of-3-and-5/multiples-of-3-and-5/main.cpp
+++ multiples-of-3-and-5/multiples-of-3-and-5/main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
 int main()
 {
+	int sum = 0;
+
+	for (int i = 3; i < 1000; ++i)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	std::cout << sum;
+
+	getchar();
+
 	return 0;
 }
